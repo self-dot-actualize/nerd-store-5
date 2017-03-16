59b827fe883c9a05c6a38ea3cb686b086f461975
Peter Jang <peter.x.jang@gmail.com>
2017-03-15 19:22:20 -0500

    Add solutions to exercises - sale message, tax, total, and discount styling

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,25 @@
 class Product < ActiveRecord::Base
+  def sale_message
+    if price.to_f < 10
+      return "Discount item!"
+    else
+      return "Everyday value!"
+    end
+  end
+
+  def tax
+    return price.to_f * 0.09
+  end
+
+  def total
+    return price.to_f + tax
+  end
+
+  def price_class_name
+    if sale_message == "Discount item!"
+      return "discount-item"
+    else
+      return ""
+    end
+  end
 end
