fd74b40b03d0b571cc2a48cceca3326b14c43a10
Peter Jang <peter.x.jang@gmail.com>
2017-03-15 19:33:35 -0500

    Restructure model and view to keep code dry and view concerns in the html

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,4 @@
 class Product < ActiveRecord::Base
-  def sale_message
-    if price.to_f < 10
-      return "Discount item!"
-    else
-      return "Everyday value!"
-    end
-  end
-
   def tax
     return price.to_f * 0.09
   end
@@ -15,11 +7,7 @@ class Product < ActiveRecord::Base
     return price.to_f + tax
   end
 
-  def price_class_name
-    if sale_message == "Discount item!"
-      return "discount-item"
-    else
-      return ""
-    end
+  def discounted?
+    return price.to_f < 10
   end
 end
