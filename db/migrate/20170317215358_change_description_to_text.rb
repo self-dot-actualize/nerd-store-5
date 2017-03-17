c4fdc8e08e93682a4af15643e1684e0b410c9465
Peter Jang <peter.x.jang@gmail.com>
2017-03-17 16:55:32 -0500

    Change description attribute to text

---
+++ db/migrate/20170317215358_change_description_to_text.rb
@@ -0,0 +1,5 @@
+class ChangeDescriptionToText < ActiveRecord::Migration[5.0]
+  def change
+    change_column :products, :description, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160515040737) do
+ActiveRecord::Schema.define(version: 20170317215358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20160515040737) do
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.string   "price"
-    t.string   "description"
+    t.text     "description"
     t.string   "image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
