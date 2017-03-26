4bd647acf06cf9e4bacf85322fe50bcb00f536b6
Peter Jang <peter.x.jang@gmail.com>
2017-03-26 10:06:43 -0500

    Add authentication login, logout and helpers

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
 
   get "/signup" => "users#new"
   post "/users" => "users#create"
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 end
