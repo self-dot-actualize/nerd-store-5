ed43a60bb406c1d44a060a5ab6a4441373041213
Peter Jang <peter.x.jang@gmail.com>
2017-03-26 10:30:04 -0500

    Save subtotal, tax, and total in order create method (refactor?)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,17 @@
 class OrdersController < ApplicationController
   def create
-    order = Order.new(quantity: params[:quantity], product_id: params[:product_id])
+    product = Product.find_by(id: params[:product_id])
+    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+
+    order = Order.new(
+      quantity: params[:quantity],
+      product_id: params[:product_id],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total
+    )
     order.save
     redirect_to "/orders/#{order.id}"
   end
