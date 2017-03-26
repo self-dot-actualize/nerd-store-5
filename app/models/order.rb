5dccbaca31d2f4a92863df2c1976e33fe48625a3
Peter Jang <peter.x.jang@gmail.com>
2017-03-26 10:54:07 -0500

    Refactor order money calculations into a model method

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,14 @@
 class Order < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :product, optional: true
+
+  def calculate_totals
+    product = Product.find_by(id: product_id)
+    subtotal = product.price * quantity
+    tax = subtotal * 0.09
+    total = subtotal + tax
+    self.subtotal = subtotal
+    self.tax = tax
+    self.total = total
+  end
 end
