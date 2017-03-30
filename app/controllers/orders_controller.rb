6e49e5dbd11d5452e12e3869730af3b5a2d39d8d
Peter Jang <peter.x.jang@gmail.com>
2017-03-29 19:34:30 -0500

    Add working order create and show actions

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,28 @@
 class OrdersController < ApplicationController
   def create
+    carted_products = current_user.carted_products.where(status: "carted")
+    subtotal = 0
+    carted_products.each do |carted_product|
+      subtotal += carted_product.quantity * carted_product.product.price
+    end
+    tax = subtotal * 0.09
+    total = subtotal + tax
+
     order = Order.new(
-      quantity: params[:quantity],
-      product_id: params[:product_id],
+      subtotal: subtotal,
+      tax: tax,
+      total: total,
       user_id: current_user.id
     )
-    order.calculate_totals
     order.save
+
+    # carted_products.each do |carted_product|
+    #   carted_product.status = "purchased"
+    #   carted_product.order_id = order.id
+    #   carted_product.save
+    # end
+    carted_products.update_all(status: "purchased", order_id: order.id)
+
     redirect_to "/orders/#{order.id}"
   end
 
