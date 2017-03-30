6cdc10f6e3fa8796c6765afa16b725b68082eb79
Peter Jang <peter.x.jang@gmail.com>
2017-03-29 19:58:07 -0500

    Refactor order money calculations to model method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,27 +1,12 @@
 class OrdersController < ApplicationController
   def create
-    carted_products = current_user.carted_products.where(status: "carted")
-    subtotal = 0
-    carted_products.each do |carted_product|
-      subtotal += carted_product.quantity * carted_product.product.price
-    end
-    tax = subtotal * 0.09
-    total = subtotal + tax
-
-    order = Order.new(
-      subtotal: subtotal,
-      tax: tax,
-      total: total,
-      user_id: current_user.id
-    )
+    order = Order.new(user_id: current_user.id)
     order.save
 
-    # carted_products.each do |carted_product|
-    #   carted_product.status = "purchased"
-    #   carted_product.order_id = order.id
-    #   carted_product.save
-    # end
+    carted_products = current_user.carted_products.where(status: "carted")
     carted_products.update_all(status: "purchased", order_id: order.id)
+
+    order.calculate_totals
 
     redirect_to "/orders/#{order.id}"
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -5,12 +5,19 @@ class Order < ApplicationRecord
   has_many :products, through: :carted_products
 
   def calculate_totals
-    product = Product.find_by(id: product_id)
-    subtotal = product.price * quantity
+    subtotal = 0
+    carted_products.each do |carted_product|
+      subtotal += carted_product.quantity * carted_product.product.price
+    end
     tax = subtotal * 0.09
     total = subtotal + tax
-    self.subtotal = subtotal
-    self.tax = tax
-    self.total = total
+    update(subtotal: subtotal, tax: tax, total: total)
+    # product = Product.find_by(id: product_id)
+    # subtotal = product.price * quantity
+    # tax = subtotal * 0.09
+    # total = subtotal + tax
+    # self.subtotal = subtotal
+    # self.tax = tax
+    # self.total = total
   end
 end
