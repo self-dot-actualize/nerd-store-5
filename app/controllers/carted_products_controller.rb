c6257dd52b13f379f06cc98c121d100cd2be5359
Peter Jang <peter.x.jang@gmail.com>
2017-03-29 19:48:03 -0500

    Add links to remove items from shopping cart

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -19,4 +19,12 @@ class CartedProductsController < ApplicationController
     carted_product.save
     redirect_to "/carted_products"
   end
+
+  def destroy
+    carted_product = CartedProduct.find_by(id: params[:id])
+    carted_product.status = "removed"
+    carted_product.save
+    flash[:success] = "Product removed!"
+    redirect_to "/carted_products"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   get "/carted_products" => "carted_products#index"
   post "/carted_products" => "carted_products#create"
+  delete "/carted_products/:id" => "carted_products#destroy"
 
   post "/orders" => "orders#create"
   get "/orders/:id" => "orders#show"
